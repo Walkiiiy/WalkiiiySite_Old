f628a883f0cdf2b3e448f7cb28703b8e4d3929fa
Walkiiiy <95386260+Walkiiiy@users.noreply.github.com>
2023-11-09 08:27:49 +0800

    Delete dist directory

--- dist/assets/works-2800ecc5.js
+++
@@ -1,1 +0,0 @@
-import{_,o as t,c,p as d,b as a,d as e}from"./index-486753a3.js";const n={},s=o=>(d("data-v-48644735"),o=o(),a(),o),i={id:"blog_box"},l=s(()=>e("h2",null,"Walkiiiy's works",-1)),r=s(()=>e("ol",{id:"blog_list"},null,-1)),p=[l,r];function h(o,u){return t(),c("div",i,p)}const k=_(n,[["render",h],["__scopeId","data-v-48644735"]]);export{k as default};
