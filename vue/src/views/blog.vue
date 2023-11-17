d7d87a51e391338d99304c5ffcd69916f847bab6
Walkiiiy <3072268178@qq.com>
2023-11-17 20:11:26 +0800

    adjust_style

--- vue/src/views/blog.vue
+++ vue/src/views/blog.vue
@@ -1,7 +1,7 @@
 <template>
     <!--博客页-->
         <div id="blog_box">
-        <h2>Walkiiiy's blog</h2>
+        <h2 style="color: black; font-size: x-large;">Walkiiiy's blog</h2>
         <ol id="blog_list">
             <!-- <li>{{blog_list}}</li> -->
             <!-- <li><router-link :to="{ path: '/blog_read', query: {blog_path:'js数组.md'}}"> js数组 </router-link> </li> -->
@@ -42,22 +42,26 @@ export default{
 }
 </script>
 <style scoped>
-  #blog_box{
-      background-color:rgba(150,150,150,0.05);
-      margin:30px;
-      padding-left: 30px;
-  }
-    #blog_list a { 
-        color:azure; /* 设置链接文字颜色 */
-        text-decoration: none; /* 去掉默认的下划线 */
-      font-size: large;  
-      }
-    
-    /* 鼠标悬停链接的样式 */
-    #blog_list a:hover {
-        color: #333; /* 设置链接文字颜色 */
-        text-decoration: underline; /* 添加下划线 */
-        cursor: pointer;/*设置手型图标*/
+#blog_box{
+    background-color:rgba(255,255,255,0.7);
+    margin:30px;
+    padding: 30px;
+}
+#blog_list li{
+    color: black;
+    margin: 7px;
+}
+#blog_list a { 
+    color:black; /* 设置链接文字颜色 */
+    text-decoration: none; /* 去掉默认的下划线 */
+    font-size:larger;
     }
-    
+
+/* 鼠标悬停链接的样式 */
+#blog_list a:hover {
+    color: #333; /* 设置链接文字颜色 */
+    text-decoration: underline; /* 添加下划线 */
+    cursor: pointer;/*设置手型图标*/
+}
+
 </style>
--- vue/src/views/home.vue
+++ vue/src/views/home.vue
@@ -5,12 +5,12 @@
     </div>
     <div style="height: 20vh;"></div>
     <div style="width: 60vw; color: black; margin: 0 auto;">
-    <div style="background-color:rgba(255, 255, 255, 0.75);border-radius: 15px;">
-            <h2 class="tit" style="padding:5px">一个简单的自我介绍</h2>
-            <p class="body" style="padding: 5px;">
+    <div style="background-color:rgba(255, 255, 255, 0.7);border-radius: 15px;">
+            <h2 class="tit" style="padding-top:15px;padding-left:15px; font-size:xx-large;font-family: 'Courier New', Courier, monospace;">一个简单的自我介绍</h2>
+            <p class="body" style="padding: 15px;font-size: large; font-family: 'Courier New', Courier, monospace;">
                 废柴大学生，专攻各种摆烂和逃课大法。喜欢打mc和瓦罗兰特，也喜欢做项目写前端（bushi）。如果你看到我的works界面是空的，大概是因为啥也不会，根本没有拿得出手的玩意（笑）。另外欢迎来github上骂我：<a href="https://github.com/Walkiiiy" style="color:darkgoldenrod;">一个普通的链接（不是瑟瑟小网站）</a>
             </p>
-            <p class="body" style="padding: 5px;">
+            <p class="body" style="padding: 15px; padding-bottom: 25px; font-size: large;font-family: 'Courier New', Courier, monospace;">
                 Idiot piece of shit,dont know nothing,can't do nothing,an absolote failure.Everyone is welcomed to insult me in my github:<a href="https://github.com/Walkiiiy" style="color:darkgoldenrod">a normal link(not pornhub)</a>
             </p>
     </div>
